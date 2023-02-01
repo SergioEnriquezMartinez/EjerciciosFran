c9cf0b4daa26c4a3f83602738197fd17cfac15e8
SergioEnriquezMartinez <sergio.enrmar.1@educa.jcyl.es>
2023-02-01 23:29:24 +0100

    tema4 y 5

---
+++ Init.java
@@ -0,0 +1,16 @@
+package edu.alonso.daw.tema5.lista;
+
+import edu.alonso.daw.tema5.Lista;
+
+public class Init {
+	
+	public static void main(String[] args) {
+		
+		Lista lista = new ListaEnlazadaSimple();
+
+		System.out.println(lista.getFirst());
+
+		
+	}
+
+}
