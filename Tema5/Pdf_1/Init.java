9e50aab93595757449baa39a9be71278d8d0ed20
SergioEnriquezMartinez <sergio.enrmar.1@educa.jcyl.es>
2023-02-13 13:00:05 +0100

    asf

---
+++ Tema5/Pdf_1/Init.java
@@ -0,0 +1,25 @@
+package edu.alonso.daw.tema5.videoclub;
+
+public class Init {
+
+	public static void main(String[] args) {
+		VideoClub vc = new VideoClub();
+		
+		Cliente cli = new Cliente(12345, "Fran");
+		
+		System.out.println(vc.aniadirCliente(cli));
+		System.out.println(vc.aniadirCliente(cli));
+//		vc.aniadirCliente(cli);
+//		vc.aniadirCliente(cli);
+		
+		Cliente clienteBuscar = vc.buscarCliente(1235);
+//		clienteBuscar.setNombre("Fran2");
+		if(clienteBuscar != null) {
+			clienteBuscar.setNombre("Fran2");
+		}
+		
+		
+		System.out.println(clienteBuscar);
+		
+	}
+}
